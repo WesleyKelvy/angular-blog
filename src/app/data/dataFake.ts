e0caefb27b6658a882a227a012c0e511147bfacc
Wesley Kelvy <126687404+WesleyKelvy@users.noreply.github.com>
2023-09-10 22:54:57 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+      'id': '1',
+      'title': 'Homem de Ferro 3, pior ou melhor?',
+      'description': 'Depois de um inimigo reduzir o mundo de Tony Stark a destroços, o Homem de Ferro precisa aprender a confiar em seus instintos para proteger aqueles que ama, especialmente sua namorada, e lutar contra seu maior medo: o fracasso.',
+      'photoCover' : 'https://e1.pxfuel.com/desktop-wallpaper/478/472/desktop-wallpaper-iron-man-3-16-9-iron-man-3-poster.jpg'
+    },
+    {
+      'id': '2',
+      'title': 'Nova serie do Disney+',
+      'description': 'Ultimo lançamento da Marvel',
+      'photoCover' : 'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg'
+    },
+]
+
